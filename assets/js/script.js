e0f368085dc62f3743fc9c7af85c766a961ea3ee
Houdychou <hchouchouh@edenschool.fr>
2024-02-16 12:32:55 +0100

    Carousel fini + burgermenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,109 @@ const btn = document.querySelector('.button');
 const carouselImg = document.querySelector('.eiffel-image');
 const titreCarousel = document.querySelector('.paris');
 const endroitCarousel = document.querySelector('.eiffel');
+const navbar = document.querySelector('.navbar');
 const txtCarousel = document.querySelector('.paris-txt');
+const nav = document.querySelector('nav');
+const navtop = document.querySelector('.navtop');
+const contact = document.querySelector('.c');
+const li = document.querySelectorAll('li');
 const carouselRight = document.querySelector('#carousel-right');
 const carouselLeft = document.querySelector('#carousel-left');
-const stars = document.querySelector('.star');
+const navbarList = document.querySelector('.nav ul');
 const firstDestination = document.querySelector('.first-destination');
+const responsiveNav = document.querySelector('.fa-bars');
+const closeButton = document.querySelector('.close-button');
 
-function handleCarousel() {
-    firstDestination.querySelector('.eiffel-image').setAttribute('src', 'assets/img/newyork.jpg')
-    titreCarousel.textContent = "New-York";
-    endroitCarousel.textContent = "Liberty Statue";
-    endroitCarousel.style.marginBottom = "0px";
-    txtCarousel.textContent = "Icon of freedom and hope, majestic, welcomes the world.";
+const IMAGES = [
+
+    {
+        "src": "assets/img/tour-eiffel.jpg",
+        "city-name": "Paris",
+        "endroit-carrousel": "Eiffel Tower",
+        "description": "A symbol of Paris, a breathtaking view, an unforgettable experience.",
+    },
+
+    {
+        "src": "assets/img/big-ben.jpg",
+        "city-name": "London",
+        "endroit-carrousel": "Big Ben Clock",
+        "description": "Majestic London icon, Big Ben, stands tall with its iconic chime",
+    },
+
+    {
+        "src": "assets/img/newyork.jpg",
+        "city-name": "New York",
+        "endroit-carrousel": "Liberty Statue",
+        "description": "Icon of freedom and hope, majestic, welcomes the world."
+    },
+]
+let currentImageIndex = 0;
+function handleCarousel(direction) {
+    if (direction === carouselLeft) {
+        currentImageIndex = (currentImageIndex - 1 + IMAGES.length) % IMAGES.length;
+    } else {
+        currentImageIndex = (currentImageIndex + 1) % IMAGES.length;
+    }
+    const currentImage = IMAGES[currentImageIndex];
+    console.log(currentImage);
+    carouselImg.src = currentImage.src;
+    titreCarousel.textContent = currentImage["city-name"];
+    endroitCarousel.textContent = currentImage["endroit-carrousel"];
+    txtCarousel.textContent = currentImage.description;
 }
 
-function prevImg() {
-    firstDestination.querySelector('.newyork').setAttribute('src', 'assets/img/tour-eiffel.jpg');
-    titreCarousel.textContent = "Paris";
-    endroitCarousel.textContent = "Eiffel Tower";
-    endroitCarousel.style.marginBottom = "34px";
-    txtCarousel.textContent = "One of the most famous landmarks in the world";
-}
+carouselRight.addEventListener('click', handleCarousel);
 
-carouselRight.addEventListener('click', function() {
-    handleCarousel();
-})
+carouselLeft.addEventListener('click', handleCarousel);
 
-carouselLeft.addEventListener('click', function() {
-    prevImg();
-})
+let navOpen = false;
+responsiveNav.addEventListener('click', function () {
+    if (!navOpen) {
+        nav.style.position = "fixed";
+        nav.style.zIndex = "2";
+        nav.style.top = "117px";
+        nav.style.right = "0px";
+        nav.style.width = "243px";
+        navtop.style.zIndex = "1";
+        navbar.style.opacity = "0";
+        contact.style.marginRight = "0";
+        navbarList.style.display = "flex";
+        navbarList.style.marginTop = "60px";
+        navbarList.style.marginRight = "40px";
+        navbarList.style.flexDirection = "column";
+        navbarList.style.alignItems = "center";
+        navbarList.style.justifyContent = "center";
+        li[0].style.paddingBottom = "40px";
+        li[1].style.paddingBottom = "40px";
+        li[2].style.paddingBottom = "40px";
+        li[3].style.paddingBottom = "40px";
+        li[4].style.paddingBottom = "40px";
+        li[5].style.paddingBottom = "40px";
+        navOpen = true;
+    } else {
+        nav.style.position = ""; 
+        nav.style.zIndex = "";
+        nav.style.top = "";
+        nav.style.right = "";
+        nav.style.width = "";
+        navtop.style.zIndex = "";
+        navbar.style.opacity = "";
+        contact.style.marginRight = "";
+        navbarList.style.display = "";
+        navbarList.style.marginTop = "";
+        navbarList.style.marginRight = "";
+        navbarList.style.flexDirection = "";
+        navbarList.style.alignItems = "";
+        navbarList.style.justifyContent = "";
+        li[0].style.paddingBottom = "";
+        li[1].style.paddingBottom = "";
+        li[2].style.paddingBottom = "";
+        li[3].style.paddingBottom = "";
+        li[4].style.paddingBottom = "";
+        li[5].style.paddingBottom = "";
+        navOpen = false; // Mettre à jour l'état de la navigation
+    }
+});
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
